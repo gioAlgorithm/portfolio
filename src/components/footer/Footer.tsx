5273b732620665cfb3bf1b871e33d7216c3c41e8
gio <95066222+gioAlgorithm@users.noreply.github.com>
2024-03-22 10:20:38 +0400

    fix the link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className={style.footerContainer}>
 
             <div className={style.innerFooter}>
-                <a href="https://www.linkedin.com/in/george-machitadze-b20b4629b" rel="noopener noreferrer" target="_blank"><BsLinkedin className={style.icons} /></a>
+                <a href="https://www.linkedin.com/in/giorgi-machitadze-b20b4629b/" rel="noopener noreferrer" target="_blank"><BsLinkedin className={style.icons} /></a>
                 <a href="https://www.youtube.com/channel/UCC713Yy8jBvpaXyCipZmRpQ" rel="noopener noreferrer" target="_blank"> <BsYoutube className={style.icons} /></a>
                 <a href="https://github.com/gioAlgorithm" rel="noopener noreferrer" target="_blank"><BsGithub className={style.icons} /></a>
             </div>
